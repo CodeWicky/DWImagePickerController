8119e244e096681a877404736ee7c4394c13be05
Wicky <codewicky@163.com>
2019-03-11 17:17:13 +0800

    first commit

---
+++ DWImagePickerController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DWImagePickerController
+//
+//  Created by Wicky on 2019/3/11.
+//  Copyright © 2019 Wicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DWImagePickerController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DWImagePickerController
+//
+//  Created by Wicky on 2019/3/11.
+//  Copyright © 2019 Wicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
