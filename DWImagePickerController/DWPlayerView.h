6af6442d165e0d2a93a3a882e584499b9edbfba0
Wicky <codewicky@163.com>
2019-05-22 01:37:40 +0800

    player

---
+++ DWImagePickerController/DWPlayerView.h
@@ -0,0 +1,17 @@
+//
+//  DWPlayerView.h
+//  DWImagePickerController
+//
+//  Created by Wicky on 2019/5/22.
+//  Copyright © 2019 Wicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DWPlayerView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DWImagePickerController/DWPlayerView.m
@@ -0,0 +1,22 @@
+//
+//  DWPlayerView.m
+//  DWImagePickerController
+//
+//  Created by Wicky on 2019/5/22.
+//  Copyright © 2019 Wicky. All rights reserved.
+//
+
+#import "DWPlayerView.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface DWPlayerView ()
+
+@property (nonatomic ,strong) AVPlayerLayer * playerLayer;
+
+@end
+
+@implementation DWPlayerView
+
+
+
+@end
